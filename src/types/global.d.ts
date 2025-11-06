560ee3b4f2a23f07e75aa11c6b0f65c07bba34ad
JohnWoo <tocwoo@gmail.com>
2025-11-06 11:12:58 +1100

    fix: declare module global

---
+++ src/types/global.d.ts
@@ -0,0 +1,20 @@
+// src/types/global.d.ts
+
+declare module 'swiper/react';
+declare module 'swiper/css';
+declare module 'swiper/css/*';
+declare module 'swiper/modules/*';
+declare module 'react-icons/*';
+
+
+declare module '*.mp4';
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
+declare module '*.webp';
+declare module '*.json';
+
+
+/// <reference types="react-scripts" />
